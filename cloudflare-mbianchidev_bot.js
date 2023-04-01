fa59f7de39659b890b416863abd1e61d473eb2e4
mbianchidev <info@mb-consulting.dev>
2023-04-01 17:55:42 +0200

    Added payload and small fix

--- cloudflare-mbianchidev_bot.js
+++ cloudflare-mbianchidev_bot.js
@@ -2,6 +2,33 @@ addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request))
 })
 
+
+/* this is the payload I get from telegram
+{
+  "update_id": 890209244,
+  "message": {
+    "message_id": 69,
+    "from": {
+      "id": 233957924,
+      "is_bot": false,
+      "first_name": "laMDA",
+      "last_name": "Bob",
+      "username": "theUsername",
+      "language_code": "en"
+    },
+    "chat": {
+      "id": 233957924,
+      "first_name": "laMDA",
+      "last_name": "Bob",
+      "username": "theUsername",
+      "type": "private"
+    },
+    "date": 1680364350,
+    "text": "Ciao"
+  }
+}
+*/
+
 async function handleRequest(request) {
   if (request.method === "POST") {
     const telegramPayload = await request.json() 
@@ -10,12 +37,9 @@ async function handleRequest(request) {
       const text = "🇬🇧 I don't have a professional telegram account anymore, if you wish to get in touch with me visit mb-consulting.dev/all-links \n 🇮🇹 Non ho più un telegram professionale, visitate mb-consulting.dev/all-links per mettervi in contatto con me"
       const url = `https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`
       const data = await fetch(url).then(resp => resp.json()) 
-      if (data.ok) {
-        return new Response("OK")
-      }
     }
     // just because I have to
-    return new Response("Error during post")
+    return new Response("OK")
   }
   return new Response("Error, only POST requests are allowed")
 }
